06af25e79e57c67e72fe0f65e235c0e31af73a07
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-09 22:32:54 +0900

    カプセル化

--- src/review4/sakitamako/com/github/ColorChart.java
+++ src/review4/sakitamako/com/github/ColorChart.java
@@ -1,5 +1,20 @@
 package review4.sakitamako.com.github;
 
 public class ColorChart {
+	private String color = null;
+	private int level = 0;
+	
+	public ColorChart(String color, int level) {
+	this.color = color;
+	this.level = level;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 }
--- src/review4/sakitamako/com/github/ColorTest.java
+++ src/review4/sakitamako/com/github/ColorTest.java
@@ -1,5 +1,14 @@
 package review4.sakitamako.com.github;
 
 public class ColorTest {
+	public static void main(String[] args) {
+		
+		ColorChart orange = new ColorChart("オレンジ", 8);
+		System.out.println(orange.getColor());
+		
+		orange.setColor("ブラウン");
+		
+		System.out.println(orange.getColor());
+	}
 
 }
