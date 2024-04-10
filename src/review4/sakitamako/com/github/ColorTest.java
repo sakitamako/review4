ce208edd7692b645fb7a3c9552fedf8bcdd58e65
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-10 22:42:23 +0900

    解決！

--- src/review4/sakitamako/com/github/ColorTest.java
+++ src/review4/sakitamako/com/github/ColorTest.java
@@ -2,12 +2,12 @@ package review4.sakitamako.com.github;
 
 public class ColorTest {
 	public static void main(String[] args) {
-		
+
 		ColorChart orange = new ColorChart("オレンジ", 8);
+//		System.out.println(orange.color); //になっていれば実行できた！
 		System.out.println(orange.getColor());
-		
+
 		orange.setColor("ブラウン");
-		
 		System.out.println(orange.getColor());
 	}
 
